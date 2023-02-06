13a9f79aaacb3e8c4046b64f6d4c39349d4a3a87
salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
2023-02-06 14:58:42 +0200

    update debug buffer tests to use anchor
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>

--- integration/solana/debug_buffer.spec.ts
+++ integration/solana/debug_buffer.spec.ts
@@ -2,156 +2,142 @@
 
 import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from '@solana/web3.js';
 import expect from 'expect';
-import { Contract, TransactionError } from '@solana/solidity';
 import { loadContract } from './setup';
+import { Program, Provider, BN } from '@project-serum/anchor';
 
 describe('DebugBuffer', function () {
     this.timeout(150000);
-
-    let contract: Contract;
+    
+    let program: Program;
+    let storage: Keypair;
     let payer: Keypair;
-    let connection: Connection;
+    let provider: Provider;
 
     before(async function () {
-        ({ contract, payer, connection } = await loadContract('Printer'));
+        ({ program, storage, payer, provider } = await loadContract('Printer'));
     });
 
     it('Prints runtime errors', async function () {
   
         try {
-            let res = await contract.functions.set_storage_bytes({ simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("set storage index out of bounds in  file: 1, line: 42,10")
-            }
+            let res = await program.methods.setStorageBytes().accounts({ dataAccount: storage.publicKey }).simulate();
+        }
+        catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain('Program log: set storage index out of bounds in  file: 1, line: 42,10');
         }
 
         try {
-            let res = await contract.functions.get_storage_bytes({ simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("storage array index out of bounds in  file: 1, line: 49,18")
-            }
+            let res = await program.methods.getStorageBytes().accounts({ dataAccount: storage.publicKey }).simulate();;
+        }
+        catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain('Program log: storage array index out of bounds in  file: 1, line: 49,18');
         }
 
         try {
-            let res = await contract.functions.pop_empty_storage({ simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("pop from empty storage array in  file: 1, line: 61,8")
-            }
+            let res = await program.methods.popEmptyStorage().accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+                expect(logs).toContain("Program log: pop from empty storage array in  file: 1, line: 61,8")
+            
         }
 
         try {
-            let res = await contract.functions.invalid_instruction({ simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("reached invalid instruction in  file: 1, line: 113,12")
-            }
+            let res = await program.methods.invalidInstruction().accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: reached invalid instruction in  file: 1, line: 113,12")
+            
         }
 
         try {
-            let res = await contract.functions.byte_cast_failure(33, { simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("bytes cast error in  file: 1, line: 121,22")
-            }
+            let res = await program.methods.byteCastFailure(new BN(33)).accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: bytes cast error in  file: 1, line: 121,22")
+            
         }
 
         try {
-            let res = await contract.functions.i_will_revert({ simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("revert encountered in  file: 1, line: 81,8")
-            }
+            let res = await program.methods.iWillRevert().accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: revert encountered in  file: 1, line: 81,8")
         }
 
         try {
-            let res = await contract.functions.assert_test(9, { simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("assert failure in  file: 1, line: 35,15")
-            }
+            let res = await program.methods.assertTest(new BN(9)).accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: assert failure in  file: 1, line: 35,15")
         }
 
         try {
-            let res = await contract.functions.write_integer_failure(1, { simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("integer too large to write in buffer in  file: 1, line: 86,17")
-            }
+            let res = await program.methods.writeIntegerFailure(new BN(1)).accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: integer too large to write in buffer in  file: 1, line: 86,17")
         }
 
         try {
-            let res = await contract.functions.write_bytes_failure(9, { simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("data does not fit into buffer in  file: 1, line: 92,17")
-            }
+            let res = await program.methods.writeBytesFailure(new BN(9)).accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: data does not fit into buffer in  file: 1, line: 92,17")
         }
 
 
         try {
-            let res = await contract.functions.read_integer_failure(2, { simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("read integer out of bounds in  file: 1, line: 97,17")
-            }
+            let res = await program.methods.readIntegerFailure(new BN(2)).accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: read integer out of bounds in  file: 1, line: 97,17")
         }
 
 
         try {
-            let res = await contract.functions.out_of_bounds(19, { simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("array out of bounds in  file: 1, line: 108,15")
-            }
+            let res = await program.methods.outOfBounds(new BN(19)).accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: array out of bounds in  file: 1, line: 108,15")
         }
 
 
         try {
-            let res = await contract.functions.trunc_failure(BigInt("999999999999999999999999"), { simulate: true });
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("truncate type overflow in  file: 1, line: 102,36")
-            }
+            let res = await program.methods.truncFailure(new BN(99999999999999) ).accounts({ dataAccount: storage.publicKey }).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs;
+            expect(logs).toContain("Program log: truncate type overflow in  file: 1, line: 102,36")
         }
 
         let child_program = new PublicKey("Cre7AzxtwSxXwU2jekYtCAQ57DkBhY9SjGDLdcrwhAo6");
         let child = Keypair.generate();
 
-        let ress = await contract.functions.create_child(child.publicKey. toBytes(), payer.publicKey.toBytes(), {
-            accounts: [child_program],
-            writableAccounts: [child.publicKey],
-            signers: [child, payer],
-            //simulate: true
-        });
-        try {
-            let res = await contract.functions.create_child(child.publicKey. toBytes(), payer.publicKey.toBytes(), {
-                accounts: [child_program],
-                //writableAccounts: [child.publicKey],
-                signers: [  payer],
-                simulate: true
-            });
 
-            console.log (res)
-        } catch (e) {
-            expect(e).toBeInstanceOf(TransactionError);
-            if (e instanceof TransactionError) {
-                expect(e.message).toEqual("contract creation failed in  file: 1, line: 71,12")
-            }
+        const signature = await program.methods.createChild(child.publicKey, payer.publicKey)
+            .accounts({ dataAccount: storage.publicKey })
+            .remainingAccounts([
+                { pubkey: child_program, isSigner: false, isWritable: false },
+                { pubkey: child.publicKey, isSigner: true, isWritable: true },
+                { pubkey: payer.publicKey, isSigner: true, isWritable: true },
+            ])
+            .signers([payer, child])
+            .rpc({ commitment: 'confirmed' });
+
+
+            const tx = await provider.connection.getTransaction(signature, { commitment: 'confirmed' });
+        try {
+            const signature = await program.methods.createChild(child.publicKey, payer.publicKey)
+            .accounts({ dataAccount: storage.publicKey })
+            .remainingAccounts([
+                { pubkey: child_program, isSigner: false, isWritable: false },
+                { pubkey: payer.publicKey, isSigner: true, isWritable: true },
+            ])
+            .signers([payer]).simulate();
+        } catch (e: any) {
+            const logs = e.simulationResponse.logs
+            expect(logs).toContain("Program log: contract creation failed in  file: 1, line: 71,12")
         }
 
         });
